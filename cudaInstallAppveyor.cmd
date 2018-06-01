e77923c1c4fc711e963c7795df138408be7e4111
Yann Lanthony <yann.lanthony@gmail.com>
2018-06-01 23:21:19 +0200

    Update cudaInstallAppveyor.cmd

--- cudaInstallAppveyor.cmd
+++ cudaInstallAppveyor.cmd
@@ -1,21 +1,22 @@
 @echo off
 echo Downloading CUDA toolkit 9
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows -FileName cuda_9.1.85_windows.exe
+https://developer.nvidia.com/compute/cuda/9.2/Prod/local_installers/cuda_9.2.88_win10
+appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.2/Prod/local_installers/cuda_9.2.88_win10 -FileName cuda_9.2.88_win10.exe
 dir
 echo Installing CUDA toolkit 9
-cuda_9.1.85_windows.exe -s nvcc_9.1 ^
-                           cublas_9.1 ^
-                           cublas_dev_9.1 ^
-                           cudart_9.1 ^
-                           curand_9.1 ^
-                           curand_dev_9.1
+cuda_9.2.88_windows.exe -s nvcc_9.2 ^
+                           cublas_9.2 ^
+                           cublas_dev_9.2 ^
+                           cudart_9.2 ^
+                           curand_9.2 ^
+                           curand_dev_9.2
 
 echo CUDA toolkit 9 installed
 
 dir "%ProgramFiles%"
 
-set CUDA_PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1
-set CUDA_BIN_PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin
+set CUDA_PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2
+set CUDA_BIN_PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin
 
 set PATH=%CUDA_BIN_PATH%;%CUDA_PATH%\libnvvp;%PATH%
 
